fa3e3917bbffab4d62b03cc3d9cbe8c805b79580
Om Pawar <om.d.pawar.214@gmail.com>
2026-01-15 20:34:03 +0530

    implemented repository to manage Users

---
+++ Server/src/main/java/com/BillingSoftware/Server/repositery/UserRepository.java
@@ -0,0 +1,14 @@
+package com.BillingSoftware.Server.repositery;
+
+import com.BillingSoftware.Server.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity,Long>{
+
+
+    Optional<UserEntity> findByUserId(String userId);
+
+    Optional<UserEntity> findByEmail(String email);
+}
